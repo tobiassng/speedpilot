e793df28a032bb24b315e81e91027b2774e40b89
Rufffy99 <max@domitrovic.com>
2025-05-13 11:21:17 +0200

    Refactor WebSocketManager to streamline speed and angle updates; modify joystick listeners accordingly

--- lib/driving_page/features/steering_joystick.dart
+++ lib/driving_page/features/steering_joystick.dart
@@ -3,6 +3,8 @@ import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:speedpilot/services/WebSocketManager.dart';
 
 class SteeringJoystickPage extends StatefulWidget {
+  const SteeringJoystickPage({super.key});
+
   @override
   _JoystickPageState createState() => _JoystickPageState();
 }
@@ -10,28 +12,25 @@ class SteeringJoystickPage extends StatefulWidget {
 class _JoystickPageState extends State<SteeringJoystickPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Joystick(
-          mode: JoystickMode.horizontal,
-          base: JoystickBase(
-            mode: JoystickMode.horizontal,
-            decoration: JoystickBaseDecoration(
-              drawOuterCircle: false,
-              color: Color.fromARGB(200, 25, 25, 25),
-              drawInnerCircle: false,
-              drawMiddleCircle: false
-            ),
-          ),
-          stick: JoystickStick(
-            decoration: JoystickStickDecoration(
-              color: Colors.red,
-            ),
-          ), listener: (StickDragDetails details) { 
-            WebSocketManager().sendDrivingData('move', details.y, details.x);
-            
-            },
+    return Joystick(
+      mode: JoystickMode.horizontal,
+      base: JoystickBase(
+        mode: JoystickMode.horizontal,
+        decoration: JoystickBaseDecoration(
+          drawOuterCircle: false,
+          color: Color.fromARGB(200, 25, 25, 25),
+          drawInnerCircle: false,
+          drawMiddleCircle: false
         ),
-      
+      ),
+      stick: JoystickStick(
+        decoration: JoystickStickDecoration(
+          color: Colors.red,
+        ),
+      ), listener: (StickDragDetails details) { 
+        WebSocketManager().updateAngle(details.x);
+        
+        },
     );
   }
 }
--- lib/services/WebSocketManager.dart
+++ lib/services/WebSocketManager.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebSocketManager {
-  
   static final WebSocketManager _instance = WebSocketManager._internal();
 
   factory WebSocketManager() {
@@ -14,8 +12,10 @@ class WebSocketManager {
   WebSocketManager._internal();
 
   WebSocketChannel? _channel;
-
   StreamSubscription? _subscription;
+
+  double _currentSpeed = 0.0;
+  double _currentAngle = 0.0;
 
   Future<void> connect(String url) async {
     final uri = Uri.parse(url);
@@ -27,14 +27,24 @@ class WebSocketManager {
     print("Nachricht gesendet: $message");
   }
 
-  void sendDrivingData(String command, double speed, double angle) {
+  void _sendDrivingCommand() {
     final Map<String, dynamic> jsonData = {
-      "command": command,
-      "speed": speed,
-      "angle": angle
+      "command": "move",
+      "speed": _currentSpeed,
+      "angle": _currentAngle
     };
     final String jsonString = jsonEncode(jsonData);
     sendMessage(jsonString);
+  }
+
+  void updateSpeed(double speed) {
+    _currentSpeed = speed;
+    _sendDrivingCommand();
+  }
+
+  void updateAngle(double angle) {
+    _currentAngle = angle;
+    _sendDrivingCommand();
   }
 
   void receiveMessage(void Function(dynamic message) onMessage) {
