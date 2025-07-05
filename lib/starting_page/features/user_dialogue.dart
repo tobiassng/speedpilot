960d28110c8580e559fd18b9af0caa300ba6a6ec
tobiassng <148528666+tobiassng@users.noreply.github.com>
2025-07-05 09:14:49 +0200

    changed something in the user dialogue

--- lib/starting_page/features/user_dialogue.dart
+++ lib/starting_page/features/user_dialogue.dart
@@ -34,7 +34,6 @@ class _AddUserDialogueState extends State<AddUserDialogue> {
         ),
       );
     }
-
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -56,7 +55,7 @@ class _AddUserDialogueState extends State<AddUserDialogue> {
           ),
           onPressed: () {
             final device =
-                CardList(nameController.text, ipaddressController.text);
+                CardList(nameController.text, "ws://${ipaddressController.text}");
             widget.addDeviceCard(device);
             Navigator.pop(context);
           },
